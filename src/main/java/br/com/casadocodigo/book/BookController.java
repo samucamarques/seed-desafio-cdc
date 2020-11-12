9626cce070cf6aa9c8832ca3289c0f9586c86517
Samuel O. Marques <samuel.marques@cvortex.io>
2020-11-11 23:46:45 -0300

    listar livros

--- src/main/java/br/com/casadocodigo/book/BookController.java
+++ src/main/java/br/com/casadocodigo/book/BookController.java
@@ -4,18 +4,23 @@ import br.com.casadocodigo.author.AuthorRepository;
 import br.com.casadocodigo.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 //Intrinsic cognitive load: 9
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/book")
 public class BookController {
 
     //1
@@ -25,7 +30,7 @@ public class BookController {
     //1
     private final AuthorRepository authorRepository;
 
-    @PostMapping("/book")
+    @PostMapping
     @ResponseStatus(HttpStatus.OK)
     @Transactional
     public Map<String, Object> create(
@@ -41,5 +46,15 @@ public class BookController {
                                 /* 1 */ authorRepository::findById));
 
         return book.toMap();
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Map<String, Object>> list() {
+        // quebrou a coesao do meu controller =(
+        return bookRepository.findAll()
+                .stream()
+                .map(Book::toMap)
+                .collect(Collectors.toList());
     }
 }
